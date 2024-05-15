dcc0ffaefa013e17f8be12160be7391c3282adbe
Muscar1a <vn.annguyenit@gmail.com>
2024-05-15 20:56:28 +0700

    update

---
+++ AimsProject/src/hust/soict/aims/media/Book.java
@@ -0,0 +1,54 @@
+package hust.soict.aims.media;
+import java.util.ArrayList;
+
+public class Book {
+    private int id;
+    private String title;
+    private String category;
+    private float cost;
+    private ArrayList<String> authors = new ArrayList<String>();
+
+    public Book() {
+        
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
+
+    public ArrayList<String> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(ArrayList<String> authors) {
+        this.authors = authors;
+    }
+}
