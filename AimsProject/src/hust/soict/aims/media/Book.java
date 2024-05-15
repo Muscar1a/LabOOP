fb714428619a34d462b019af1351382210f240ab
An Nguyen <vn.annguyenit@gmail.com>
2024-05-16 00:06:49 +0700

    update

--- AimsProject/src/hust/soict/aims/media/Book.java
+++ AimsProject/src/hust/soict/aims/media/Book.java
@@ -1,47 +1,12 @@
 package hust.soict.aims.media;
+
 import java.util.ArrayList;
 
 public class Book {
-    private int id;
-    private String title;
-    private String category;
-    private float cost;
     private ArrayList<String> authors = new ArrayList<String>();
 
     public Book() {
-        
-    }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public float getCost() {
-        return cost;
-    }
-
-    public void setCost(float cost) {
-        this.cost = cost;
     }
 
     public ArrayList<String> getAuthors() {
@@ -50,5 +15,29 @@ public class Book {
 
     public void setAuthors(ArrayList<String> authors) {
         this.authors = authors;
+    }
+
+    public void addAuthor(String authorName) {
+        for (int i = 0; i < authors.size(); i++) {
+            if (authors.get(i).equals(authorName)) {
+                System.out.println("The author is already in the list.");
+                return;
+            } else {
+                authors.add(authorName);
+                System.out.println("The author has been added.");
+                return;
+            }
+        }
+    }
+
+    public void removeAuthor(String authorName) {
+        for (int i = 0; i < authors.size(); i++) {
+            if (authors.get(i).equals(authorName)) {
+                authors.remove(i);
+                System.out.println("The author has been removed.");
+                return;
+            }
+        }
+        System.out.println("The author is not in the list.");
     }
 }
